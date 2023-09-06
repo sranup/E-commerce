52d83ec0263633f0293581dede9df8fc2d1187f4
sranup <sranup007@gmail.com>
2023-09-06 22:03:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const server = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const cookieParser=require('cookie-parser')
+
+const productsRouter = require("./routes/Products");
+const categoriesRouter = require("./routes/Categories");
+const brandsRouter = require("./routes/Brands");
+const usersRouter = require("./routes/Users");
+const authRouter = require("./routes/Auth");
+const cartRouter = require("./routes/Cart");
+const ordersRouter = require("./routes/Order");
+const { User } = require("./model/User");
+const { isAuth, sanitizeUser, cookieExtractor } = require("./services/common");
+
+const SECRET_KEY = "SECRET_KEY";
+
+server.use(cookieParser())
+const opts = {};
+opts.jwtFromRequest = cookieExtractor
+opts.secretOrKey = SECRET_KEY;
+//middlewares
+
+server.use(express.static('build'))
+
+server.use(
+  session({
+    secret: "Keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+server.use(passport.authenticate("session"));
+
+server.use(
+  cors({
+    exposedHeaders: ["X-Total-Count"],
+  })
+);
+server.use(express.json()); // to parse req.body
+server.use("/products", isAuth(), productsRouter.router);
+server.use("/categories",isAuth(), categoriesRouter.router);
+server.use("/brands",isAuth(), brandsRouter.router);
+server.use("/users",isAuth(), usersRouter.router);
+server.use("/auth", authRouter.router);
+server.use("/cart",isAuth(), cartRouter.router);
+server.use("/orders",isAuth(), ordersRouter.router);
+
+passport.use(
+  "local",
+  new LocalStrategy(
+    {usernameField:'email'},
+    async function (email, password, done) {
+    try {
+      const user = await User.findOne({ email:email }).exec();
+      console.log(email,password,user)
+      if (!user) {
+        done(null, false, { message: "invalid credentials" });
+      }
+      crypto.pbkdf2(
+        password,
+        user.salt,
+        310000,
+        32,
+        "sha256",
+        async function (err, hashedPassword) {
+          if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+            return done(null, false, { message: "invalid credentials" });
+          }
+          const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
+          done(null, {id:user.id,role:user.role,token});
+        }
+      );
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+
+passport.use(
+  "jwt",
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    console.log({ jwt_payload });
+    try {
+      const user = await User.findById( jwt_payload.id);
+      if (user) {
+        return done(null, sanitizeUser(user));
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+passport.serializeUser(function (user, cb) {
+  console.log("serialize", user);
+  process.nextTick(function () {
+    return cb(null, { id: user.id, role: user.role });
+  });
+});
+
+// this changes session variable req.user when called from authorized request
+
+passport.deserializeUser(function (user, cb) {
+  console.log("de-serialize", user);
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
+  console.log("database connected");
+}
+
+server.listen(8080, () => {
+  console.log("server started");
+});
